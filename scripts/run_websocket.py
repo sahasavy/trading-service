718346d32264e701418fdb658ded480ac6147292
Sourav Saha <sahasavy@gmail.com>
2025-07-21 15:39:14 +0530

    BRN-2: feat: Adding some fixes to the auth and market-data modules

--- scripts/run_websocket.py
+++ scripts/run_websocket.py
@@ -1,4 +1,19 @@
-from src.market_data.websocket_ticks import connect_websocket
+from src.market_data.websocket_ticks import start_websocket
+import yaml
+
+CONFIG_PATH = "config/config.yaml"
+
+def read_config():
+    with open(CONFIG_PATH, 'r') as file:
+        return yaml.safe_load(file)
 
 if __name__ == "__main__":
-    connect_websocket()
+    config = read_config()
+    user_input = input("Enter symbols (comma-separated, press enter for default list from Config): ").strip()
+
+    if user_input:
+        symbols = [sym.strip().upper() for sym in user_input.split(',')]
+    else:
+        symbols = config['websocket']['default_symbols']
+
+    start_websocket(symbols)
---
+++ scripts/scheduler/run_websocket_scheduler.py
@@ -0,0 +1,27 @@
+from apscheduler.schedulers.blocking import BlockingScheduler
+from src.market_data.websocket_ticks import start_websocket
+from src.utils.time_utils import get_current_ist_time
+import yaml
+
+CONFIG_PATH = "config/config.yaml"
+
+
+def read_config():
+    with open(CONFIG_PATH, 'r') as file:
+        return yaml.safe_load(file)
+
+
+def scheduled_websocket():
+    config = read_config()
+    symbols = config['websocket']['default_symbols']
+    print(f"🚀 Scheduled websocket started at: {get_current_ist_time()}")
+    start_websocket(symbols)
+
+
+scheduler = BlockingScheduler(timezone='Asia/Kolkata')
+scheduler.add_job(scheduled_websocket, 'cron', day_of_week='mon-fri', hour=9, minute=0)
+
+if __name__ == "__main__":
+    print("🕒 Starting websocket scheduler.")
+    scheduled_websocket()       # initial run immediately
+    # scheduler.start()           # We can uncomment it when we want to run a full-fledged scheduler
